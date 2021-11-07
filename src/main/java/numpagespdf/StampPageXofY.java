5d43341a9dc50eb648b637075aadd61d54419753
Haliz <foril2n1@rambler.ru>
2021-11-07 18:30:48 +0300

    Добавил itext через мавен 5-ю версию

--- src/main/java/numpagespdf/StampPageXofY.java
+++ src/main/java/numpagespdf/StampPageXofY.java
@@ -1,12 +1,9 @@
 package numpagespdf;
 
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfReader;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.VerticalAlignment;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
 
@@ -16,24 +13,24 @@ public class StampPageXofY {
     public static final String SRC = "C:\\Users\\Igor\\Downloads\\восточный.pdf";
 
     public static void main(String[] args) throws Exception {
-        File file = new File(DEST);
-        file.getParentFile().mkdirs();
-
-        new StampPageXofY().manipulatePdf(DEST);
+//        File file = new File(DEST);
+//        file.getParentFile().mkdirs();
+//
+//        new StampPageXofY().manipulatePdf(DEST);
     }
 
-    protected void manipulatePdf(String dest) throws Exception {
-        PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC), new PdfWriter(dest));
-        Document doc = new Document(pdfDoc);
-
-        int numberOfPages = pdfDoc.getNumberOfPages();
-        for (int i = 1; i <= numberOfPages; i++) {
-
-            // Write aligned text to the specified by parameters point
-            doc.showTextAligned(new Paragraph(String.format("page %s of %s", i, numberOfPages)),
-                    559, 806, i, TextAlignment.RIGHT, VerticalAlignment.TOP, 0);
-        }
-
-        doc.close();
-    }
+//    protected void manipulatePdf(String dest) throws Exception {
+//        PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC), new PdfWriter(dest));
+//        Document doc = new Document(pdfDoc);
+//
+//        int numberOfPages = pdfDoc.getNumberOfPages();
+//        for (int i = 1; i <= numberOfPages; i++) {
+//
+//            // Write aligned text to the specified by parameters point
+//            doc.showTextAligned(new Paragraph(String.format("page %s of %s", i, numberOfPages)),
+//                    559, 806, i, TextAlignment.RIGHT, VerticalAlignment.TOP, 0);
+//        }
+//
+//        doc.close();
+//    }
 }
