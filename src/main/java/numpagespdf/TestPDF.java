86864f16b791db807a79f17993f94911cf255d3c
Haliz <foril2n1@rambler.ru>
2021-11-26 00:46:00 +0300

    Нумерует каждый отчет

--- src/main/java/numpagespdf/TestPDF.java
+++ src/main/java/numpagespdf/TestPDF.java
@@ -3,6 +3,7 @@ package numpagespdf;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.property.TextAlignment;
@@ -12,16 +13,22 @@ import java.io.IOException;
 
 public class TestPDF {
     public static void main(String[] args) throws IOException {
+        PdfReader reader = new PdfReader("C:\\projects\\pages_numerator\\src\\main\\java\\numpagespdf\\Пример 1.pdf");
         PdfDocument pdfDoc =
-                new PdfDocument(new PdfReader("C:\\projects\\pages_numerator\\src\\main\\java\\numpagespdf\\Пример 1.pdf"),
+                new PdfDocument(reader,
                         new PdfWriter("C:\\projects\\pages_numerator\\src\\main\\java\\numpagespdf\\Запись 1.pdf"));
         Document doc = new Document(pdfDoc);
 // add content
         int numberOfPages = pdfDoc.getNumberOfPages();
+        int k = 1;
         for (int i = 1; i <= numberOfPages; i++) {
-
+            String text = PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i)); // Выцепляет текст из страницы (Потом поставить i)
             // Write aligned text to the specified by parameters point
-            doc.showTextAligned(new Paragraph(String.format("%s", i)),
+            String pattern = "ГЛАВГОССТРОЙНАДЗОР МОСКОВСКОЙ ОБЛАСТИ";
+            if (text.contains(pattern)) {
+                k = 1;
+            }
+            doc.showTextAligned(new Paragraph(String.format("%s", k++)),
                     300, 10, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
         }
 
