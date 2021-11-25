c44c675c0360d308546c27a33e739f9cf8b8a544
Haliz <foril2n1@rambler.ru>
2021-11-25 23:31:00 +0300

    Простая вставка номера страниц

--- src/main/java/numpagespdf/TestPDF.java
+++ src/main/java/numpagespdf/TestPDF.java
@@ -3,6 +3,10 @@ package numpagespdf;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
 
 import java.io.IOException;
 
@@ -11,7 +15,16 @@ public class TestPDF {
         PdfDocument pdfDoc =
                 new PdfDocument(new PdfReader("C:\\projects\\pages_numerator\\src\\main\\java\\numpagespdf\\Пример 1.pdf"),
                         new PdfWriter("C:\\projects\\pages_numerator\\src\\main\\java\\numpagespdf\\Запись 1.pdf"));
+        Document doc = new Document(pdfDoc);
 // add content
+        int numberOfPages = pdfDoc.getNumberOfPages();
+        for (int i = 1; i <= numberOfPages; i++) {
+
+            // Write aligned text to the specified by parameters point
+            doc.showTextAligned(new Paragraph(String.format("%s", i)),
+                    300, 10, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
+        }
+
         pdfDoc.close();
     }
 
