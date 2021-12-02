14047b626dc9d3d3ea34393448d17f3cef41dedf
Haliz <foril2n1@rambler.ru>
2021-12-02 23:45:30 +0300

    Сделал простенький интерфейс.

---
+++ src/main/java/swing/NumeratorGUI.java
@@ -0,0 +1,23 @@
+package swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class NumeratorGUI {
+    public static void main(String[] args) {
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame("Нумератор страниц отчетов(v1.0)");
+        frame.setBounds(550, 250, 700, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+//        Icon clicker = new ImageIcon("src/main/java/swing/Expert.png");
+        JLabel label = new JLabel("Нажмите на кнопку для выбора одного или нескольких "
+                + "отчетов в формате PDF", 0);
+        frame.add(panel);
+        panel.add(label);
+        JButton button = new JButton("Выбрать");
+        panel.add(button);
+    }
+
+}
